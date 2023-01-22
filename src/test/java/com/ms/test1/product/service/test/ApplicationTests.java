fa13d68153f9f94829a5124500936575c2dd5cad
mehedee-hassa <mehedee.hassan@outlook.com>
2023-01-22 20:06:09 +0900

    project created

---
+++ src/test/java/com/ms/test1/product/service/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.test1.product.service.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
